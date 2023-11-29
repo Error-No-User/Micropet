9ffe2bb7df97f5c3ce0f183e00a9302c29a779bc
Error-No-User <150885314+Error-No-User@users.noreply.github.com>
2023-11-29 14:52:11 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ def on_pin_pressed_p0():
             music.PlaybackMode.UNTIL_DONE)
 input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
 
+def on_button_pressed_a():
+    control.reset()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_gesture_screen_down():
+    global health
+    health = 0
+input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
+
 def on_pin_pressed_p1():
     global health, Food
     if health < 10 and health > 0:
@@ -14,8 +23,14 @@ def on_pin_pressed_p1():
             music.PlaybackMode.UNTIL_DONE)
         health += 1
         Food += -1
+        basic.show_icon(IconNames.SILLY)
         basic.pause(1000)
 input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+def on_gesture_shake():
+    global health
+    health += -2
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
 Money = 0
 health = 0
@@ -24,6 +39,13 @@ Food = 5
 Money = 8
 
 def on_forever():
+    global Money
+    if health > 6:
+        Money += 1
+        basic.pause(10000)
+basic.forever(on_forever)
+
+def on_forever2():
     global health
     if health <= 10 and health > 6:
         health += -1
@@ -34,9 +56,9 @@ def on_forever():
     if health <= 3 and health > 0:
         health += -1
         basic.pause(2500)
-basic.forever(on_forever)
+basic.forever(on_forever2)
 
-def on_forever2():
+def on_forever3():
     if health > 6:
         basic.show_icon(IconNames.HAPPY)
     if health <= 6 and health > 3:
@@ -47,4 +69,4 @@ def on_forever2():
         basic.show_icon(IconNames.SKULL)
         music._play_default_background(music.built_in_playable_melody(Melodies.FUNERAL),
             music.PlaybackMode.UNTIL_DONE)
-basic.forever(on_forever2)
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,55 +1,81 @@
-input.onPinPressed(TouchPin.P0, function () {
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
     if (Money > 0) {
         Food += 1
         Money += -1
         music.play(music.tonePlayable(988, music.beat(BeatFraction.Eighth)), music.PlaybackMode.UntilDone)
     }
+    
 })
-input.onPinPressed(TouchPin.P1, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    control.reset()
+})
+input.onGesture(Gesture.ScreenDown, function on_gesture_screen_down() {
+    
+    health = 0
+})
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
     if (health < 10 && health > 0) {
         music.play(music.tonePlayable(330, music.beat(BeatFraction.Quarter)), music.PlaybackMode.UntilDone)
         health += 1
         Food += -1
+        basic.showIcon(IconNames.Silly)
         basic.pause(1000)
     }
+    
+})
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    health += -2
 })
 let Money = 0
 let health = 0
 health = 10
 let Food = 5
 Money = 8
-basic.forever(function () {
-    if (health > 6) {
-        basic.showIcon(IconNames.Happy)
-    }
-    if (health <= 6 && health > 3) {
-        basic.showIcon(IconNames.Confused)
-    }
-    if (health <= 3 && health > 0) {
-        basic.showIcon(IconNames.Sad)
-    }
-    if (health <= 0) {
-        basic.showIcon(IconNames.Skull)
-        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.UntilDone)
-    }
-})
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     if (health > 6) {
         Money += 1
         basic.pause(10000)
     }
+    
 })
-basic.forever(function () {
+basic.forever(function on_forever2() {
+    
     if (health <= 10 && health > 6) {
         health += -1
         basic.pause(10000)
     }
+    
     if (health <= 6 && health > 3) {
         health += -1
         basic.pause(5000)
     }
+    
     if (health <= 3 && health > 0) {
         health += -1
         basic.pause(2500)
     }
+    
+})
+basic.forever(function on_forever3() {
+    if (health > 6) {
+        basic.showIcon(IconNames.Happy)
+    }
+    
+    if (health <= 6 && health > 3) {
+        basic.showIcon(IconNames.Confused)
+    }
+    
+    if (health <= 3 && health > 0) {
+        basic.showIcon(IconNames.Sad)
+    }
+    
+    if (health <= 0) {
+        basic.showIcon(IconNames.Skull)
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.UntilDone)
+    }
+    
 })
